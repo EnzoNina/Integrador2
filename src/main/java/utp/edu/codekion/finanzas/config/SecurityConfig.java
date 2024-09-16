9452bcb3c46e9ec0263fbe10a19ac2e742d04c10
Enzo Nina <ninaenzo70@gmail.com>
2024-09-16 17:15:45 -0500

    fix cors flutter

--- src/main/java/utp/edu/codekion/finanzas/config/SecurityConfig.java
+++ src/main/java/utp/edu/codekion/finanzas/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         //Establecemos los metodos permitidos
-        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.addAllowedOrigin("http://localhost:*");
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.addAllowedMethod("*");
         corsConfiguration.setAllowCredentials(true); // Para permitir el envio de credenciales
