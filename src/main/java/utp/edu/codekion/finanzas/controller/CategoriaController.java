753291f420c7597cb45480b9814ed20c48f8ab38
Enzo Nina <ninaenzo70@gmail.com>
2024-09-30 17:36:34 -0500

    fix categoria

--- src/main/java/utp/edu/codekion/finanzas/controller/CategoriaController.java
+++ src/main/java/utp/edu/codekion/finanzas/controller/CategoriaController.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/y")
+@RequestMapping("/categoria")
 @RequiredArgsConstructor
 public class CategoriaController {
 
