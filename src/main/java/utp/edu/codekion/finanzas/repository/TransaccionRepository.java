1a4a313b735713a4d0f517db47dc9c60660ab987
Enzo Nina <ninaenzo70@gmail.com>
2024-10-28 17:08:52 -0500

    fix dashboard egreso ingreso

--- src/main/java/utp/edu/codekion/finanzas/repository/TransaccionRepository.java
+++ src/main/java/utp/edu/codekion/finanzas/repository/TransaccionRepository.java
@@ -60,6 +60,6 @@ public interface TransaccionRepository extends JpaRepository<Transacciones, Inte
     @Query("SELECT SUM(t.monto) FROM Transacciones t WHERE t.fechaTransaccion BETWEEN ?1 AND ?2 and t.idCategoria.idTipoTra.id = 1")
     BigDecimal totalIngresosEntreFechas(LocalDate fechaInicio, LocalDate fechaFin);
 
-    @Query("SELECT SUM(t.monto) FROM Transacciones t WHERE t.fechaTransaccion BETWEEN ?1 AND ?2 and t.idCategoria.idTipoTra.id = 1")
+    @Query("SELECT SUM(t.monto) FROM Transacciones t WHERE t.fechaTransaccion BETWEEN ?1 AND ?2 and t.idCategoria.idTipoTra.id = 2")
     BigDecimal totalEgresosEntreFechas(LocalDate fechaInicio, LocalDate fechaFin);
 }
